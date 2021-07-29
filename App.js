3d6a459f748dc3a26d88b282168c495980ef58de
Chao Chen <chaochen@live.com>
2021-07-29 15:02:20 -0400

    created firebase context and added bottom tab icons

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { firebase, FieldValue } from './lib/firebase';
+import FirebaseContext from './context/firebase';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 import TempScreen from './components/temp';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Store" component={TempScreen} />
-        <Tab.Screen name="Leagues" component={TempScreen} />
-        <Tab.Screen name="Friends" component={TempScreen} />
-        <Tab.Screen name="Profile" component={TempScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <FirebaseContext.Provider value={{ firebase, FieldValue }}>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Leagues" labeled={false} barStyle={{ backgroundColor: 'teal' }}>
+          <Tab.Screen name="Store" component={TempScreen} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="shopping" color={color} size={26} />
+            )
+          }} />
+          <Tab.Screen name="Leagues" component={TempScreen} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="finance" color={color} size={26} />
+            )
+          }} />
+          <Tab.Screen name="Friends" component={TempScreen} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account-group" color={color} size={26} />
+            )
+          }} />
+          <Tab.Screen name="Profile" component={TempScreen} options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            )
+          }} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </FirebaseContext.Provider>
   );
 }
