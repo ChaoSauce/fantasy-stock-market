ec72d210e76bfee8baf7be310f6abcc661aed93d
Chao Chen <chaochen@live.com>
2021-07-29 15:49:51 -0400

    added SafeAreaProvider and SafeAreaView to fix content position on devices with notches

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { firebase, FieldValue } from './lib/firebase';
@@ -14,30 +15,32 @@ export default function App() {
   return (
     <FirebaseContext.Provider value={{ firebase, FieldValue }}>
       <StatusBar />
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName="Leagues" labeled={false} barStyle={{ backgroundColor: 'teal' }}>
-          <Tab.Screen name="Store" component={TempScreen} options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="shopping" color={color} size={26} />
-            )
-          }} />
-          <Tab.Screen name="Leagues" component={TempScreen} options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="finance" color={color} size={26} />
-            )
-          }} />
-          <Tab.Screen name="Friends" component={TempScreen} options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="account-group" color={color} size={26} />
-            )
-          }} />
-          <Tab.Screen name="Profile" component={TempScreen} options={{
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="account" color={color} size={26} />
-            )
-          }} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Tab.Navigator initialRouteName="Leagues" labeled={false} barStyle={{ backgroundColor: 'teal' }}>
+            <Tab.Screen name="Store" component={TempScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="shopping" color={color} size={26} />
+              )
+            }} />
+            <Tab.Screen name="Leagues" component={TempScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="finance" color={color} size={26} />
+              )
+            }} />
+            <Tab.Screen name="Friends" component={TempScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="account-group" color={color} size={26} />
+              )
+            }} />
+            <Tab.Screen name="Profile" component={TempScreen} options={{
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="account" color={color} size={26} />
+              )
+            }} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </FirebaseContext.Provider>
   );
 }
--- components/temp.js
+++ components/temp.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function TempScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello!</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Hello!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
