c8fd659785d3faf0ef9167717e23fb201a5e8e0c
Chao Chen <chaochen@live.com>
2021-07-30 14:46:23 -0400

    set up handle login

--- components/login.js
+++ components/login.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import tw from 'tailwind-react-native-classnames';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import FirebaseContext from '../context/firebase';
 
 export default function Login({ navigation }) {
+  const firebase = useContext(FirebaseContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      setPassword('');
+      setError(error.message);
+    }
+  }
+
   return (
     <SafeAreaView style={tw`container flex-1 justify-center mx-auto max-w-screen-md items-center h-screen`}>
       <View style={tw`flex container w-4/5 items-center justify-center`}>
         <Text
-          style={tw`font-bold text-xl mb-5`}
+          style={tw`font-bold text-xl mb-3`}
         >Fantasy Stock Market</Text>
+        <Text style={tw`mb-4 text-red-700`}>{error}</Text>
         <TextInput
           style={tw`p-3 border-2 w-full rounded-sm items-center`}
           placeholder="Email address"
@@ -21,6 +37,7 @@ export default function Login({ navigation }) {
         />
         <TouchableOpacity
           style={tw`bg-green-500 p-3 mt-5 font-bold rounded-full w-3/5 items-center justify-center`}
+          onPress={handleLogin}
         >
           <Text style={tw`text-white font-bold`}>Log In</Text>
         </TouchableOpacity>
