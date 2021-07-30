35da8de564f19b95d0f440ecc58ea316dcab4f64
Chao Chen <chaochen@live.com>
2021-07-30 11:41:10 -0400

    using tailwindcss for styling

--- components/temp.js
+++ components/temp.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'tailwind-react-native-classnames';
 
 export default function TempScreen() {
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <SafeAreaView style={tw`flex-1 justify-center items-center`}>
         <Text>Hello!</Text>
     </SafeAreaView>
   );
