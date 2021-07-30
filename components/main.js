1f556c9804ffc86e413cc2b64e53f360718d54b4
Chao Chen <chaochen@live.com>
2021-07-30 14:34:57 -0400

    store tab color in state

--- components/main.js
+++ components/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import TempScreen from './temp';
@@ -6,37 +6,39 @@ import TempScreen from './temp';
 const Tab = createMaterialBottomTabNavigator();
 
 export default function Main() {
+  const [tabColor, setTabColor] = useState('rgba(16, 185, 129, 1)');
+
   return (
     <Tab.Navigator initialRouteName="Feed" labeled={true} >
       <Tab.Screen name="Feed" component={TempScreen} options={{
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="home" color={color} size={26} inactiveColor="white" />
         ),
-        tabBarColor: 'rgba(16, 185, 129, 1)'
+        tabBarColor: tabColor
       }} />
       <Tab.Screen name="Store" component={TempScreen} options={{
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="shopping" color={color} size={26} />
         ),
-        tabBarColor: 'rgba(16, 185, 129, 1)'
+        tabBarColor: tabColor
       }} />
       <Tab.Screen name="Leagues" component={TempScreen} options={{
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="finance" color={color} size={26} />
         ),
-        tabBarColor: 'rgba(16, 185, 129, 1)'
+        tabBarColor: tabColor
       }} />
       <Tab.Screen name="Friends" component={TempScreen} options={{
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="account-group" color={color} size={26} />
         ),
-        tabBarColor: 'rgba(16, 185, 129, 1)'
+        tabBarColor: tabColor
       }} />
       <Tab.Screen name="Profile" component={TempScreen} options={{
         tabBarIcon: ({ color }) => (
           <MaterialCommunityIcons name="account" color={color} size={26} />
         ),
-        tabBarColor: 'rgba(16, 185, 129, 1)'
+        tabBarColor: tabColor
       }} />
     </Tab.Navigator>
   )
