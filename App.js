ffe72f6b7f4d1327ccc8de9c89c1303145fcae2d
Chao Chen <chaochen@live.com>
2021-07-30 15:57:51 -0400

    organized components to use LoggedInUser context

--- App.js
+++ App.js
@@ -1,48 +1,12 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from 'react';
 import { firebase, FieldValue } from './lib/firebase';
 import FirebaseContext from './context/firebase';
-import LoggedInUserContext from './context/logged-in-user';
-import userAuthListener from './hooks/user-auth-listener';
-
-import Main from './components/main';
-import Login from './components/login';
-import SignUp from './components/sign-up';
-
-const Stack = createStackNavigator();
+import Index from './index';
 
 export default function App() {
-  const { user } = userAuthListener();
-
-  if (!user) {
-    return (
-      <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-        <SafeAreaProvider>
-          <StatusBar />
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-              <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-              <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </SafeAreaProvider>
-      </FirebaseContext.Provider>
-    )
-  }
-
   return (
     <FirebaseContext.Provider value={{ firebase, FieldValue }}>
-      <SafeAreaProvider>
-        <StatusBar />
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Main">
-            <Stack.Screen name="Main" component={Main} options={{headerShown: false}} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
+      <Index />
     </FirebaseContext.Provider>
-  );
+  )
 }
---
+++ index.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import userAuthListener from './hooks/user-auth-listener';
+import LoggedInUserContext from './context/logged-in-user';
+import tw from 'tailwind-react-native-classnames';
+
+import Main from './components/main';
+import Login from './components/login';
+import SignUp from './components/sign-up';
+
+const Stack = createStackNavigator();
+
+export default function Index() {
+  const [loaded, setLoaded] = useState(false);
+  const { user } = userAuthListener();
+
+  useEffect(() => {
+    if (user) {
+      setLoaded(true);
+    }
+  }, [user])
+
+  if (!loaded) {
+    return <View style={tw`flex-1 items-center justify-center`}>
+      <Text>Loading...</Text>
+    </View>
+  }
+
+  if (!user) {
+    return (
+      <SafeAreaProvider>
+        <StatusBar />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+            <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    )
+  }
+
+  return (
+    <LoggedInUserContext.Provider value={{ user }}>
+      <SafeAreaProvider>
+        <StatusBar />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={Main} options={{headerShown: false}} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </LoggedInUserContext.Provider>
+  );
+  
+}
