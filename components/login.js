87aeb95a4442e0832b1111faffac8d876b41fde2
Chao Chen <chaochen@live.com>
2021-07-30 18:38:53 -0400

    fixed text input dismiss keyboard once again...

--- components/login.js
+++ components/login.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import tw from 'tailwind-react-native-classnames';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FirebaseContext from '../context/firebase';
 
@@ -18,46 +18,48 @@ export default function Login({ navigation }) {
       setPassword('');
       setError(error.message);
     }
-  }
+  };
 
   return (
-    <SafeAreaView style={tw`container flex-1 justify-center mx-auto max-w-screen-md items-center`}>
-      <View style={tw`flex container w-4/5 items-center justify-center`}>
-        <Text
-          style={tw`font-bold text-xl mb-3`}
-        >Fantasy Stock Market</Text>
-        <Text style={tw`mb-4 text-red-700`}>{error}</Text>
-        <TextInput
-          style={tw`p-3 border-2 w-full rounded-sm items-center`}
-          placeholder="Email address"
-          onChangeText={(email) => setEmail(email)}
-          value={email}
-        />
-        <TextInput
-          style={tw`mt-4 p-3 border-2 w-full rounded-sm items-center`}
-          placeholder="Password"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-          value={password}
-        />
-        <TouchableOpacity
-          disabled={isInvalid}
-          style={tw.style(`bg-green-500 p-3 mt-5 font-bold rounded-full w-3/5 items-center justify-center`, isInvalid && 'opacity-50')}
-          onPress={handleLogin}
-        >
-          <Text style={tw`text-white font-bold`}>Log In</Text>
-        </TouchableOpacity>
-        <View style={tw`flex w-full mt-5 items-center`}>
-          <Text>Don't have an account? {` `}
-            <Text
-              style={tw`text-blue-700`}
-              onPress={() => navigation.navigate('SignUp')}
-            >
-              Sign Up
+    <SafeAreaView style={tw`container flex-1 justify-center mx-auto max-w-screen-sm items-center`}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={tw`flex-1 container w-full items-center justify-center`}>
+          <Text
+            style={tw`font-bold text-xl mb-3`}
+          >Fantasy Stock Market</Text>
+          <Text style={tw`mb-4 mx-4 text-red-700`}>{error}</Text>
+          <TextInput
+            style={tw`p-3 border-2 w-4/5 rounded-sm items-center`}
+            placeholder="Email address"
+            onChangeText={(email) => setEmail(email)}
+            value={email}
+          />
+          <TextInput
+            style={tw`mt-4 p-3 border-2 w-4/5 rounded-sm items-center`}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+            value={password}
+          />
+          <TouchableOpacity
+            disabled={isInvalid}
+            style={tw.style(`bg-green-500 p-3 mt-5 font-bold rounded-full w-2/5 items-center justify-center`, isInvalid && 'opacity-50')}
+            onPress={handleLogin}
+          >
+            <Text style={tw`text-white font-bold`}>Log In</Text>
+          </TouchableOpacity>
+          <View style={tw`flex w-full mt-5 items-center`}>
+            <Text>Don't have an account? {` `}
+              <Text
+                style={tw`text-blue-700`}
+                onPress={() => navigation.navigate('SignUp')}
+              >
+                Sign Up
+              </Text>
             </Text>
-          </Text>
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   )
 }
--- components/sign-up.js
+++ components/sign-up.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import tw from 'tailwind-react-native-classnames';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FirebaseContext from '../context/firebase';
 import { doesUsernameExist } from '../services/firebase';
@@ -42,55 +42,57 @@ export default function SignUp({ navigation }) {
   }
 
   return (
-    <SafeAreaView style={tw`container flex-1 justify-center mx-auto max-w-screen-md items-center`}>
-      <View style={tw`flex container w-4/5 items-center justify-center`}>
-        <Text
-          style={tw`font-bold text-xl mb-5`}
-        >Fantasy Stock Market</Text>
-        <Text style={tw`mb-4 text-red-700`}>{error}</Text>
-        <TextInput
-          style={tw`p-3 border-2 w-full rounded-sm items-center`}
-          placeholder="Username"
-          onChangeText={(username) => setUsername(username)}
-          value={username}
-        />
-        <TextInput
-          style={tw`mt-4 p-3 border-2 w-full rounded-sm items-center`}
-          placeholder="Full name"
-          onChangeText={(fullName) => setFullName(fullName)}
-          value={fullName}
-        />
-        <TextInput
-          style={tw`mt-4 p-3 border-2 w-full rounded-sm items-center`}
-          placeholder="Email address"
-          onChangeText={(email) => setEmail(email)}
-          value={email}
-        />
-        <TextInput
-          style={tw`mt-4 p-3 border-2 w-full rounded-sm items-center`}
-          placeholder="Password"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-          value={password}
-        />
-        <TouchableOpacity
-          disabled={isInvalid}
-          style={tw.style(`bg-green-500 p-3 mt-5 font-bold rounded-full w-3/5 items-center justify-center`, isInvalid && 'opacity-50')}
-          onPress={handleSignUp}
-        >
-          <Text style={tw`text-white font-bold`}>Sign Up</Text>
-        </TouchableOpacity>
-        <View style={tw`flex w-full mt-5 items-center`}>
-          <Text>Already have an account? {` `}
-            <Text
-              style={tw`text-blue-700`}
-              onPress={() => navigation.navigate('Login')}
-            >
-              Log In
+    <SafeAreaView style={tw`container flex-1 justify-center mx-auto max-w-screen-sm items-center`}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={tw`flex-1 container w-full items-center justify-center`}>
+          <Text
+            style={tw`font-bold text-xl mb-5`}
+          >Fantasy Stock Market</Text>
+          <Text style={tw`mb-4 text-red-700`}>{error}</Text>
+          <TextInput
+            style={tw`p-3 border-2 w-4/5 rounded-sm items-center`}
+            placeholder="Username"
+            onChangeText={(username) => setUsername(username)}
+            value={username}
+          />
+          <TextInput
+            style={tw`mt-4 p-3 border-2 w-4/5 rounded-sm items-center`}
+            placeholder="Full name"
+            onChangeText={(fullName) => setFullName(fullName)}
+            value={fullName}
+          />
+          <TextInput
+            style={tw`mt-4 p-3 border-2 w-4/5 rounded-sm items-center`}
+            placeholder="Email address"
+            onChangeText={(email) => setEmail(email)}
+            value={email}
+          />
+          <TextInput
+            style={tw`mt-4 p-3 border-2 w-4/5 rounded-sm items-center`}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+            value={password}
+          />
+          <TouchableOpacity
+            disabled={isInvalid}
+            style={tw.style(`bg-green-500 p-3 mt-5 font-bold rounded-full w-2/5 items-center justify-center`, isInvalid && 'opacity-50')}
+            onPress={handleSignUp}
+          >
+            <Text style={tw`text-white font-bold`}>Sign Up</Text>
+          </TouchableOpacity>
+          <View style={tw`flex w-full mt-5 items-center`}>
+            <Text>Already have an account? {` `}
+              <Text
+                style={tw`text-blue-700`}
+                onPress={() => navigation.navigate('Login')}
+              >
+                Log In
+              </Text>
             </Text>
-          </Text>
+          </View>
         </View>
-      </View>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   )
 }
