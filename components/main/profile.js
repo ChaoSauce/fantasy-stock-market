8884ebff1ffde454906343446483d7d468708f21
Chao Chen <chaochen@live.com>
2021-07-30 21:45:25 -0400

    added getUserByUserId service for current logged in user info

--- components/main/profile.js
+++ components/main/profile.js
@@ -1,20 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FirebaseContext from '../../context/firebase';
 import LoggedInUserContext from '../../context/logged-in-user';
+import { getUserByUserId } from '../../services/firebase';
 
 export default function Profile() {
   const { firebase } = useContext(FirebaseContext);
   const { user } = useContext(LoggedInUserContext);
+  const [currentUser, setCurrentUser] = useState(null);
 
-  console.log(user);
+  useEffect(() => {
+    async function getCurrentUser() {
+      const [result] = await getUserByUserId(user.uid);
+      setCurrentUser(result);
+    }
+
+    getCurrentUser();
+  }, [user])
+  
 
   return (
     <SafeAreaView style={tw`container flex-1 justify-center mx-auto max-w-screen-sm items-center`}>
-      <View>
-        <Text>{user.username}</Text>
+      <View style={tw`border-b-2 mb-5 w-3/5 pb-1 items-center`}>
+        <Text>{currentUser?.fullName}</Text>
       </View>
       <TouchableOpacity
         style={tw`bg-red-600 p-2 w-2/5 rounded-full items-center justify-center`}
